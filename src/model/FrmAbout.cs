86a5606a5323cd36ec5e160e7500d68df513f45d
phong <thanhphong41099@gmail.com>
2025-09-24 13:11:24 +0700

    Initial commit

---
+++ src/model/FrmAbout.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VLeague.src.helper;
+
+namespace VLeague.src.model
+{
+    public partial class FrmAbout : Form
+    {
+        public FrmAbout()
+        {
+            InitializeComponent();
+
+            try
+            {
+                string licenseKey = LicenseKeyHandler.readLicenseLocalFile();
+                LicenseKeyHandler.writeLicenseLocalFile(licenseKey);
+                string expirationDate = LicenseKeyHandler.onGetValueOfLicenseByKey(licenseKey, "expirationDate");
+                DateTimeOffset dateOfExpired = LicenseKeyHandler.onGetExpirationDate(expirationDate);
+                string expirationDate2 = dateOfExpired.Date.ToString("dd/MM/yyyy");
+
+
+                txtAbout.Text = 
+                    $"Version 1.0.4. Expiration: ({expirationDate2})\n" +
+                    $"Copyright © VTVBroadcom MS.\n" +
+                    $"All rights reserved http://vtvms.vn.\n\n" +
+                    $"Update on: 24/06/2025.\n\n" +
+                    $"--------------------------------------------------";
+            }
+            catch 
+            {
+                txtAbout.Text =
+                $"Version 1.0.4. Ex: No License Key\n" +
+                $"Copyright © VTVBroadcom MS.\n" +
+                $"All rights reserved http://vtvms.vn\n\n." +
+                $"--------------------------------------------------";
+            }
+        }
+
+        private void FrmAbout_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
